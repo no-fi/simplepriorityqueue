b2fb16fc9dce97e4e92b54ba5b9257b3b7b0c2d3
Christopher Kiel <no-fi@users.noreply.github.com>
2016-11-07 22:24:02 -0600

    Adds index.js and test.js files and sets up basic test command

---
+++ test.js
@@ -0,0 +1,8 @@
+var expect = require('chai').expect;
+var PriorityQueue = require('./index.js');
+
+describe("Simple Priority Queue", function () {
+  it("should fail", function () {
+    expect(true).to.equal(false);
+  });
+});
